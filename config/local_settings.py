0aac2ace7eb6723b7e4ff223872efeb93b5350db
koshihikari77 <inadakoro@gmail.com>
2019-10-09 07:43:26 +0900

    fix settings.py

--- config/local_settings.py
+++ config/local_settings.py
@@ -2,6 +2,7 @@ from .settings import *
 
 DEBUG = True
 INTERNAL_IPS = ['127.0.0.1']
+SECRET_KEY = '*78qidg^^u=($&djk8d)$m)u8s@xge&_6w2vdp3#l@4cm7r$rz'
 
 if DEBUG:
     def show_toolbar(request):
--- config/settings.py
+++ config/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*78qidg^^u=($&djk8d)$m)u8s@xge&_6w2vdp3#l@4cm7r$rz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
